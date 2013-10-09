f59236e4144e2a5680f51cc36b1ceca665f8cc40
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:12:05 -0500

    I did workday 6

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,22 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+        Dim num As Integer = 1
+        Dim sqr As Integer
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Do While num <= 9
+
+            num = num + 1
+            sqr = num ^ 2
+            lstResult.Items.Add("The perfect square is " & sqr)
+
+
+
+        Loop
+
+
+        lstResult.Items.Add("These are the first 10 perfect squares!")
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,23 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim CurPop As Double
+        Dim CurYEar As Integer
 
-        MessageBox.Show("Not implemented yet", "Error")
+        CurYEar = 2012
+        CurPop = 7000
+
+        Do While (CurPop >= 6)
+            CurYEar = (CurYEar - 50)
+            CurPop = CurPop / 2
+
+
+        Loop
+
+
+
+        MessageBox.Show("The population wwas less than 6 million in " & CurYEar, "Error")
 
     End Sub
+
 End Class
